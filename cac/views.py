326466aa1c14af236a762c81ce9de9c11a2cfa77
Nicolas Sicolo <sicolo2208@gmail.com>
2022-10-14 18:28:52 -0300

    genero vistas

--- cac/views.py
+++ cac/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
 
 #views.py contiene toda la logica del negocio.
+
+
+def saludo( request ):
+    return HttpResponse("Hola a todos!")
+
+def saludo_modificado( request, edad ):
+    documento = "<html><body><h2>Hola tenes %s anios"%(edad)
+    return HttpResponse(documento)    
--- proyecto_django_2022/urls.py
+++ proyecto_django_2022/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from cac.views import saludo_modificado
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("saludo/<int:edad>", saludo_modificado)
 ]
+
